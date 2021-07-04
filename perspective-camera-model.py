d19b97247cbb05763478b03e21205a61542f132f
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-04 19:10:54 +0100

    Create perspective-camera-model.py

---
+++ perspective-camera-model.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def main():
+    # Intrinsic parameters
+    alpha = 925  # Magnification factor in x' direction
+    beta = 740  # Magnification factor in y' direction
+    offset_x = 244  # Offset from the top-left corner to the principal point, along the x-axis / pixels
+    offset_y = 180  # Offset from the top-left corner to the principal point, along the y-axis / pixels
+
+    projection_operator = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0]
+    ])
+
+    intrinsic_camera_parameters = np.array([
+        [alpha, 0, offset_x],
+        [0, beta, offset_y],
+        [0, 0, 1]
+    ])
+
+    X, Y, Z = 10, 10, 500
+
+    coords = (1/Z) * np.matmul(
+        intrinsic_camera_parameters,
+        np.matmul(projection_operator, np.array([[X], [Y], [Z], [1]]))
+    )
+    print(coords)
+
+
+if __name__ == "__main__":
+    main()
