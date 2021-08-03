5f5e6fc0ee1be92d10fe30d5ca1e915b61b281fe
Dominic Swaine <dominic.swaine@gmail.com>
2021-08-03 16:12:21 +0100

    Adapt moving average calculation as appropriate

---
+++ imageDifferencing.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def image_differencing(series):
+    for before, after in zip(series[:-1], series[1:]):
+        diff = np.array(before) - np.array(after)
+        print(diff)
+
+# Adapt moving average calculation as appropriate
+def background_subtraction(series, beta=0.5):
+    background = np.zeros(np.array(series[0].shape))
+    for image in series:
+        background = (1 - beta) * background + beta * image
+        diff = np.array(image) - background
+        print("Background", background)
+        print("Diff", diff)
+
+
+def main():
+    series = [
+        np.array([[190, 200, 90, 110, 90]]),
+        np.array([[110, 170, 160, 70, 70]]),
+        np.array([[100, 60, 170, 200, 90]]),
+        np.array([[90, 100, 100, 190, 190]])
+    ]
+    image_differencing(series)
+    background_subtraction(series)
+
+
+if __name__ == "__main__":
+    main()
