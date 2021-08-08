9568542acbf620326c566442eaca5530937a9806
Dominic Swaine <dominic.swaine@gmail.com>
2021-08-08 20:15:15 +0100

    Fix bugs in region growing, region merging

--- region_growing.py
+++ region_growing.py
@@ -17,10 +17,10 @@ def region_growing(arr, metric, offsets):
         for pos in region:
             if pos in unassigned:
                 unassigned.remove(pos)
-                feature_vector = arr[pos[1], pos[0], :]
-                out[pos[1], pos[0]] = region_index
+                feature_vector = arr[pos[0], pos[1], :]
+                out[pos[0], pos[1]] = region_index
                 for neighbour_pos in neighbouring_positions(pos, arr, offsets):
-                    neighbour_feature_vector = arr[neighbour_pos[1], neighbour_pos[0], :]
+                    neighbour_feature_vector = arr[neighbour_pos[0], neighbour_pos[1], :]
                     if metric(feature_vector, neighbour_feature_vector) and neighbour_pos in unassigned:
                         region.append(neighbour_pos)
 
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     image = np.array([
         [[5, 10, 15], [10, 15, 30], [10, 10, 25]],
         [[10, 10, 15], [5, 20, 15], [10, 5, 30]],
-        [[10, 10, 15], [30, 10, 5], [30, 10, 10]]
+        [[5, 5, 15], [30, 10, 5], [30, 10, 10]]
     ])
     hvdOffsets = horizontalVerticalAndDiagonal
     print(region_growing(image, lambda a, b: sum_of_absolute_differences(a, b) < 12, hvdOffsets))
--- region_merging.py
+++ region_merging.py
@@ -8,27 +8,27 @@ def region_merging(arr, metric, offsets):
     out = np.zeros((x_dim, y_dim))
 
     region_index = 1
-    for y in range(y_dim):
-        for x in range(x_dim):
-            out[y, x] = region_index
+    for x in range(x_dim):
+        for y in range(y_dim):
+            out[x, y] = region_index
             region_index += 1
 
     region_index = 1
     while region_index < (x_dim * y_dim):
         to_expand = []
 
-        for y in range(y_dim):
-            for x in range(x_dim):
-                if out[y, x] == region_index:
+        for x in range(x_dim):
+            for y in range(y_dim):
+                if out[x, y] == region_index:
                     to_expand.append((x, y))
 
         while len(to_expand) >= 1:
             pos = to_expand.pop()
-            feature_vector = arr[pos[1], pos[0], :]
-            out[pos[1], pos[0]] = region_index
+            feature_vector = arr[pos[0], pos[1]]
+            out[pos[0], pos[1]] = region_index
             for neighbour_pos in neighbouring_positions(pos, arr, offsets):
-                if out[neighbour_pos[1], neighbour_pos[0]] > region_index:
-                    neighbour_feature_vector = arr[neighbour_pos[1], neighbour_pos[0], :]
+                if out[neighbour_pos[0], neighbour_pos[1]] > region_index:
+                    neighbour_feature_vector = arr[neighbour_pos[0], neighbour_pos[1]]
                     if metric(feature_vector, neighbour_feature_vector):
                         to_expand.append(neighbour_pos)
         region_index += 1
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     img = np.array([
         [[5, 10, 15], [10, 15, 30], [10, 10, 25]],
         [[10, 10, 15], [5, 20, 15], [10, 5, 30]],
-        [[10, 10, 15], [30, 10, 5], [30, 10, 10]]
+        [[5, 5, 15], [30, 10, 5], [30, 10, 10]]
     ])
     hvdOffsets = horizontalVerticalAndDiagonal
     print(region_merging(img, lambda a, b: sum_of_absolute_differences(a, b) < 12, hvdOffsets))
