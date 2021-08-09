ee1271791033d145265b81301a358ab86469c498
Dominic Swaine <dominic.swaine@gmail.com>
2021-08-09 23:05:37 +0100

    Add unit tests to distance_metrics.py

--- distance_metrics.py
+++ distance_metrics.py
@@ -1,8 +1,15 @@
+# python -m unittest harris_corner_detector
+
 import numpy as np
+import unittest
 
 
 def sum_of_absolute_differences(a, b):
     return np.sum(np.abs(a - b))
+
+
+def sum_of_squared_differences(a, b):
+    return np.sum(np.square(a - b))
 
 
 def cross_correlation(a, b):
@@ -29,3 +36,59 @@ def correlation_coefficient(a, b):
 
 def euclidean_distance(a, b):
     return np.sqrt(np.sum(np.square(a - b)))
+
+
+class TestDistanceMetrics(unittest.TestCase):
+    T1 = np.array([
+        [1, 1, 1],
+        [1, 0, 0],
+        [1, 1, 1],
+    ])
+
+    T2 = np.array([
+        [1, 0, 0],
+        [1, 0, 0],
+        [1, 1, 1],
+    ])
+
+    T3 = np.array([
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+    ])
+
+    I = np.array([
+        [1, 1, 1],
+        [1, 0, 0],
+        [1, 1, 1],
+    ])
+
+    def test_cross_correlation(self):
+        self.assertEqual(cross_correlation(self.T1, self.I), 7)
+        self.assertEqual(cross_correlation(self.T2, self.I), 5)
+        self.assertEqual(cross_correlation(self.T3, self.I), 7)
+
+    def test_normalised_cross_correlation(self):
+        self.assertEqual(round(normalised_cross_correlation(self.T1, self.I), 2), 1)
+        self.assertEqual(round(normalised_cross_correlation(self.T2, self.I), 2), 0.85)
+        self.assertEqual(round(normalised_cross_correlation(self.T3, self.I), 2), 0.94)
+
+    def test_correlation_coefficient(self):
+        self.assertEqual(round(correlation_coefficient(self.T1, self.I), 2), 1)
+        self.assertEqual(round(correlation_coefficient(self.T2, self.I), 2), 0.60)
+        self.assertEqual(round(correlation_coefficient(self.T3, self.I), 2), 0.66)
+
+    def test_sum_of_absolute_differences(self):
+        self.assertEqual(round(sum_of_absolute_differences(self.T1, self.I), 2), 0)
+        self.assertEqual(round(sum_of_absolute_differences(self.T2, self.I), 2), 2)
+        self.assertEqual(round(sum_of_absolute_differences(self.T3, self.I), 2), 1)
+
+    def test_sum_of_squared_differences(self):
+        self.assertEqual(round(sum_of_squared_differences(self.T1, self.I), 2), 0)
+        self.assertEqual(round(sum_of_squared_differences(self.T2, self.I), 2), 2)
+        self.assertEqual(round(sum_of_squared_differences(self.T3, self.I), 2), 1)
+
+    def test_euclidean_distance(self):
+        self.assertEqual(round(euclidean_distance(self.T1, self.I), 2), 0)
+        self.assertEqual(round(euclidean_distance(self.T2, self.I), 2), 1.41)
+        self.assertEqual(round(euclidean_distance(self.T3, self.I), 2), 1)
